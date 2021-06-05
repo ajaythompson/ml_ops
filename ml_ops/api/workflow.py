d3e5911457d0724e5e5b4af576fe13fd4d99b1eb
ajay <ajaythompson@gmail.com>
2021-06-05 20:59:12 +0530

    added workflow module

--- ml_ops/api/workflow.py
+++ ml_ops/api/workflow.py
@@ -8,69 +8,26 @@ from pyspark.sql.session import SparkSession
 import uuid
 from flask_cors import CORS
 
-app = Flask(__name__)
-CORS(app)
-# app.config.from_envvar('MLOPS_CONFIG_PATH')
-app.config
 
-# Set the secret key to some random bytes. Keep this really secret!
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-
-
-def get_global_spark_session():
-    if 'spark' not in g:
-        conf = SparkConf()
-        conf.set("spark.master", "local[*]")
-        conf.set("spark.app.name", "tests_helper")
-        conf.set("spark.sql.shuffle.partitions", "8")
-        g.sc = SparkSession.builder.config(conf=conf).getOrCreate()
-    return g.sc
-
-
-def get_session_id():
-    if '_ml_id' not in session:
-        session['_ml_id'] = str(uuid.uuid4())
-
-
-def get_spark_session():
-    session_id = get_session_id()
-
-    if 'ss' not in g:
-        g.sessions = {}
-
-    if session_id not in g.sessions:
-        g.sessions[session_id] = {}
-
-    if 'spark_session' not in g.sessions[session_id]:
-        g.sessions[session_id]['spark_session'] = get_global_spark_session() \
-            .newSession()
-
-    return g.sessions[session_id]['spark_session']
 
 
 def get_workflow():
     return session['workflow']
 
 
-def create_workflow():
-    session['workflow'] = SparkWorkflowManager().get_config()
-    return session['workflow']
-
-
 WORKFLOW_BASE_PATH = '/workflow'
-PROCESSORS_BASE_PATH = f'{WORKFLOW_BASE_PATH}/processors'
-PROCESSOR_BASE_PATH = f'{WORKFLOW_BASE_PATH}/processor'
-RELATION_BASE_PATH = f'{WORKFLOW_BASE_PATH}/relation'
-WORKFLOW_RUN_PATH = f'{PROCESSOR_BASE_PATH}/run'
-WORKFLOW_PREVIEW_PATH = f'{PROCESSOR_BASE_PATH}/preview'
 
 
-@app.route(f'{WORKFLOW_BASE_PATH}', methods=['GET', 'POST'])
+@app.route
 def workflow():
+    if request.method == 'GET':
+        return spar
+
+
+@app.route(f'{WORKFLOW_BASE_PATH}', methods=['POST'])
+def create_workflow():
     if request.method == 'POST':
-        return create_workflow()
-    else:
-        return get_workflow()
+        return SparkWorkflowManager().create_workflow()
 
 
 @app.route(f'{PROCESSORS_BASE_PATH}', methods=['GET'])
